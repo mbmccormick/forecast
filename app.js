682ebe6eb6c65f3cef66b2f2d151434d63f53066
Matt McCormick <mbmccormick@users.noreply.github.com>
2023-11-08 22:29:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const contentDiv = document.getElementById('content');
 
-  fetch('https://cors-anywhere.herokuapp.com/https://forecast.weather.gov/product.php?site=LSX&issuedby=LSX&product=AFD&format=CI&version=1&glossary=1')
+  fetch('https://forecast.weather.gov/product.php?site=LSX&issuedby=LSX&product=AFD&format=CI&version=1&glossary=1')
     .then(response => response.text())
     .then(data => {
       contentDiv.innerHTML = data; // This will not work as is due to CORS and formatting, but shows the basic idea
