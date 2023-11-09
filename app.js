d56eabc7f2554efaf1d62ac673f7a836728f2044
Matt McCormick <mbmccormick@users.noreply.github.com>
2023-11-08 22:51:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://forecast.weather.gov/product.php?site=LSX&issuedby=LSX&product=AFD&format=txt&version=1&glossary=1')
     .then(response => response.text())
     .then(data => {
-      contentDiv.innerHTML = data;
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(data, 'text/html');
+      const glossaryContent = doc.querySelector('#glossaryProduct');
+
+      if (glossaryContent) {
+        contentDiv.innerHTML = glossaryContent.innerHTML;
+      } else {
+        contentDiv.innerHTML = 'Glossary content not found.';
+      }
     })
     .catch(error => {
       contentDiv.innerHTML = 'Failed to load data.';
